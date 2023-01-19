64657b634612ac600cbe22acbfcce53f96e73186
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2023-01-19 23:44:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/ultimacrews-project/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
