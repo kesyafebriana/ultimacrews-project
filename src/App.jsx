daf5b019fcca5bc910b3766e0fc12d11d8f560a6
Gerald Imanuel <gerald.imanuel@student.umn.ac.id>
2023-01-20 13:48:46 +0700

    deleting null path

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ function App() {
   const [login, setLogin] = useState("auth/sign-in");
 
   return (
-    <Switch>
-      <Routes>
-        <Route path="/dashboard/*" element={<Dashboard />} />
-        <Route path="/auth/*" element={<Auth />} />
-        <Route path="*" element={<Navigate to={login} replace />} />
-      </Routes>
-    </Switch>
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      {/* <Route path="*" element={<Navigate to={login} replace />} /> */}
+    </Routes>
   );
 }
 
